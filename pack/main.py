daf4d4414614fcfd87265fb2cbd0442aef759e38
elochka <akulinaem@gmail.com>
2014-02-18 22:32:48 +0400

    pic folder commit

--- pack/main.py
+++ pack/main.py
@@ -1,7 +1,7 @@
 from PIL import Image
 
 __author__ = 'Asus'
-image_file = 'me.jpg'
+image_file = '../TestData/me.jpg'
 img = Image.open(image_file)
 # get the image's width and height in pixels
 width, height = img.size
